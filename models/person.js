dff355c8c5bede658df3fa7b08e23696fd0eb126
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2023-09-13 11:49:54 +0100

    made emails and phone numbers unique

--- models/person.js
+++ models/person.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const personSchema = new mongoose.Schema({
     name: { type: String, required: true },
     age: { type: String, required: true },
-    email: { type: String, required: true },
-    phone_no: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    phone_no: { type: String, required: true, unique: true }
 });
 
 const Person = mongoose.model("Person", personSchema);
